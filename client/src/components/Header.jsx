a5e2708de17aaec6fb2f6fddfe95c8fd798ac774
akshat-315 <akshat315.sh@gmail.com>
2024-04-14 10:51:14 +0530

    fixed header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -13,12 +13,12 @@ const Header = () => {
         to="/"
         className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
       >
-        <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-400 rounded-lg text-white">
+        <span className="px-2 py-2 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-400 rounded-lg text-white">
           MERN Blog
         </span>
       </Link>
 
-      <form>
+      <form className="w-1/4">
         <TextInput
           type="text"
           placeholder="Search..."
@@ -26,23 +26,23 @@ const Header = () => {
           className="hidden lg:inline"
         />
       </form>
-      <Button className="w-12 h-10 lg:hidden" color="gray" pill>
+      <Button className="w-16 h-10 lg:hidden" color="gray" pill>
         <AiOutlineSearch />
       </Button>
 
       <div className="flex gap-2 md:order-2">
         <Button className="w-12 h-10 hidden sm:inline" color="gray" pill>
-          {<FaMoon />}
+          <FaMoon />
         </Button>
         <Link to="/sign-in">
-          <Button gradientDuoTone="purpleToBlue" outline>
+          <Button gradientDuoTone="purpleToBlue" className="text-lg" outline>
             Sign In
           </Button>
         </Link>
         <Navbar.Toggle />
       </div>
 
-      <Navbar.Collapse>
+      <Navbar.Collapse className="pb-6">
         <Navbar.Link active={path === "/"} as={"div"}>
           <Link to="/" className="text-xl">
             Home
